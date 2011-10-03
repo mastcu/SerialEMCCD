5e9f333b789c779ac6f3c6241ed4756926963013
mast <devnull@localhost>
2011-10-03 16:32:01 +0000

    Allow for different target names

--- SerialEMCCDps.mk
+++ SerialEMCCDps.mk
@@ -4,14 +4,30 @@ SerialEMCCDps.dll: dlldata.obj SerialEMCCD_p.obj SerialEMCCD_i.obj
 		dlldata.obj SerialEMCCD_p.obj SerialEMCCD_i.obj \
 		kernel32.lib rpcns4.lib rpcrt4.lib oleaut32.lib uuid.lib \
 
+SEMCCDps-GMS2-32.dll: dlldata.obj SerialEMCCD_p.obj SerialEMCCD_i.obj
+	link /dll /out:SEMCCDps-GMS2-32.dll /def:SerialEMCCDps.def \
+		dlldata.obj SerialEMCCD_p.obj SerialEMCCD_i.obj \
+		kernel32.lib rpcns4.lib rpcrt4.lib oleaut32.lib uuid.lib \
+
+SEMCCDps-GMS2-64.dll: dlldata.obj SerialEMCCD_p.obj SerialEMCCD_i.obj
+	link /dll /out:SEMCCDps-GMS2-64.dll /def:SerialEMCCDps.def \
+		dlldata.obj SerialEMCCD_p.obj SerialEMCCD_i.obj \
+		kernel32.lib rpcns4.lib rpcrt4.lib oleaut32.lib uuid.lib \
+
 .c.obj:
-	cl /c /Ox /DWIN32 /D_WIN32_WINNT=0x0400 /DREGISTER_PROXY_DLL \
+	cl /c /Ox /DWIN32 /D_WIN32_WINNT=0x0500 /DREGISTER_PROXY_DLL \
 		$<
 
 clean:
 	@del SerialEMCCDps.dll
 	@del SerialEMCCDps.lib
 	@del SerialEMCCDps.exp
+	@del SEMCCDps-GMS2-32.dll
+	@del SEMCCDps-GMS2-32.lib
+	@del SEMCCDps-GMS2-32.exp
+	@del SEMCCDps-GMS2-64.dll
+	@del SEMCCDps-GMS2-64.lib
+	@del SEMCCDps-GMS2-64.exp
 	@del dlldata.obj
 	@del SerialEMCCD_p.obj
 	@del SerialEMCCD_i.obj
