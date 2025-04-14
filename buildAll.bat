2fadd0ee6a700cf2ccf530f7792cf0be3f878e44
mast <none@none>
2025-04-13 19:54:06 -0600

    oops it skipped to the last one

--- buildAll.bat
+++ buildAll.bat
@@ -1,7 +1,6 @@
 Rem Builds all versions of the plugin, run as cmd /C buildAll.bat
 
 set SAVEPATH=%PATH%
-GOTO Label
 set PATH=c:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\BIN\amd64;C:\windows\Microsoft.NET\Framework64\v4.0.30319;C:\windows\Microsoft.NET\Framework64\v3.5;c:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\VCPackages;c:\Program Files (x86)\Microsoft Visual Studio 10.0\Common7\IDE;c:\Program Files (x86)\Microsoft Visual Studio 10.0\Common7\Tools;C:\Program Files (x86)\HTML Help Workshop;C:\Program Files (x86)\Microsoft SDKs\Windows\v7.0A\bin\NETFX 4.0 Tools\x64;C:\Program Files (x86)\Microsoft SDKs\Windows\v7.0A\bin\x64;C:\Program Files (x86)\Microsoft SDKs\Windows\v7.0A\bin;%PATH%
 set LIB=c:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\LIB\amd64;c:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\ATLMFC\LIB\amd64;C:\Program Files (x86)\Microsoft SDKs\Windows\v7.0A\lib\x64
 set INCLUDE=c:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\INCLUDE;c:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\ATLMFC\INCLUDE;C:\Program Files (x86)\Microsoft SDKs\Windows\v7.0A\include
@@ -56,7 +55,6 @@ set DM_PLUGIN_SUFFIX=_NoBCG
 msbuild /t:Rebuild /p:Configuration=GMS3.5-IOMP /p:Platform=x64 SerialEMCCD14.vcxproj
 if %errorlevel% neq 0 exit /b %errorlevel%
 
-:Label
 set MSVS2019=C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional
 set WINKIT10=C:\Program Files (x86)\Windows Kits\10
 set PATH=%MSVS2019%\VC\Tools\MSVC\14.29.30133\bin\HostX64\x64;%MSVS2019%\Common7\IDE\VC\VCPackages;%MSVS2019%\Common7\IDE\CommonExtensions\Microsoft\TestWindow;%MSVS2019%\Common7\IDE\CommonExtensions\Microsoft\TeamFoundation\Team Explorer;%MSVS2019%\MSBuild\Current\bin\Roslyn;%MSVS2019%\Team Tools\Performance Tools\x64;%MSVS2019%\Team Tools\Performance Tools;C:\Program Files (x86)\Microsoft Visual Studio\Shared\Common\VSPerfCollectionTools\vs2019\\x64;C:\Program Files (x86)\Microsoft Visual Studio\Shared\Common\VSPerfCollectionTools\vs2019\;C:\Program Files (x86)\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.8 Tools\x64\;C:\Program Files (x86)\HTML Help Workshop;%MSVS2019%\Common7\Tools\devinit;%WINKIT10%\bin\10.0.19041.0\x64;%WINKIT10%\bin\x64;%MSVS2019%\\MSBuild\Current\Bin;C:\windows\Microsoft.NET\Framework64\v4.0.30319;%MSVS2019%\Common7\IDE\;%MSVS2019%\Common7\Tools\;%MSVS2019%\Common7\IDE\CommonExtensions\Microsoft\CMake\CMake\bin;%MSVS2019%\Common7\IDE\CommonExtensions\Microsoft\CMake\Ninja
